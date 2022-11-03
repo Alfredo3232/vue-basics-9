4a0492f6e5f18e0856286178a536fb32be6b58e9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-03 14:52:13 -0600

    add rating buttons on form and properly get input data

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: userNameValidity === 'invalid' }">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" @blur="validateInput" />
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput" />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
 
     <div class="form-control">
@@ -56,6 +57,10 @@
     </div>
 
     <div class="form-control">
+      <rating-control v-model="rating"></rating-control>
+    </div>
+
+    <div class="form-control">
       <input type="checkbox" id="confirm=terms" name="confirm-terms" />
       <label for="confirm-terms">Agree to terms of use?</label>
     </div>
@@ -66,7 +71,12 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
+
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       userName: '',
@@ -74,7 +84,9 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      rating: null,
+      userNameValidity: 'pending'
     };
   },
   methods: {
@@ -100,6 +112,17 @@ export default {
 
       console.log('Confirm?', this.confirm);
       this.confirm = false;
+
+      console.log('Rating');
+      console.log(this.rating);
+      this.rating = null;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 }
@@ -117,6 +140,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
